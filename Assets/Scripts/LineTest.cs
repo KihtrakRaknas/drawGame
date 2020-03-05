6cae867d8f1be8b1cdebeef541be86f450ad5fc8
Rishi J <flameclaw02@gmail.com>
2020-03-05 10:27:28 -0500

    Line Functionality

--- Assets/Scripts/LineTest.cs
+++ Assets/Scripts/LineTest.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class LineTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    LineRenderer lr;
+    public GameObject start;
+    public GameObject end;
+    public Material Black;
+    float dist;
+    float counter = 0;
+    float drawSpeed = 2f;
     void Start()
     {
-        
+        lr = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        lr.SetPosition(0, start.transform.position);
+        lr.startWidth = 0.45f;
+        lr.endWidth = 0.45f;
+        dist = Vector3.Distance(start.transform.position, end.transform.position);
+        lr.materials[0] = Black;
+        if (counter < dist)
+        {
+            counter += 0.1f / drawSpeed;
+            float x = Mathf.Lerp(0, dist, counter);
+            Vector3 pointAlongLine = x * Vector3.Normalize(end.transform.position - start.transform.position) + start.transform.position;
+            lr.SetPosition(1, pointAlongLine);
+        }
     }
 }
