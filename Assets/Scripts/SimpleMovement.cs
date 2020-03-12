bc62e889a044a37f616cadb3aacc5351f380ef59
Rishi J <flameclaw02@gmail.com>
2020-03-12 10:29:48 -0400

    VirtualTask

---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    float speedx = 0f;
+    float speedz = 3f;
+    public static bool collide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            speedx = -2f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speedx = 2f;
+        }
+        else
+        {
+            speedx = 0f;
+        }
+        transform.Translate(new Vector3(speedx * Time.deltaTime, 0f, speedz * Time.deltaTime));
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("obstacle"))
+        {
+            collide = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("obstacle"))
+        {
+            collide = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/SimpleObstacle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float counter=0;
+    bool moved = false;
+    bool collided = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (SimpleMovement.collide)
+        {
+            counter = 0;
+            if (counter < 1)
+            {
+                transform.Translate(0f, 1f * Time.deltaTime, 0f);
+                collided = true;
+            }
+        }
+        if (counter > 1 && collided)
+        {
+            moved = true;
+
+        }
+        if(!SimpleMovement.collide && moved)
+        {
+            counter = 0;
+            if (counter < 0.3)
+            {
+                transform.Translate(0f, -1f * Time.deltaTime, 0f);
+            }
+        }
+    }
+}
