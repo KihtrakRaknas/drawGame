4f0213ee6401d781d3a02c1c22a99fb393ddd5a4
KihtrakRaknas <karthikandsankar@gmail.com>
2020-03-12 19:51:04 -0400

    perfected the dynamically generated collider!

--- Assets/Scripts/LineTest.cs
+++ Assets/Scripts/LineTest.cs
@@ -12,45 +12,86 @@ public class LineTest : MonoBehaviour
     float counter = 0;
     float drawSpeed = 2f;
     float width = 0.45f;
-    Vector3 startPos;
-    Vector3 endPos;
+    Vector3 oldPos = new Vector3(0, 0, 0);
+    int pointNum = 0;
 
     void Start()
     {
         lr = GetComponent<LineRenderer>();
-        
-        startPos = start.transform.position;
-        endPos = end.transform.position;
+
+        Vector3 startPos = start.transform.position;
+        Vector3 endPos = end.transform.position;
 
         lr.startWidth = width;
         lr.endWidth = width;
         lr.materials[0] = Black;
-        addLineSegment(startPos, endPos);
+        lr.SetPosition(0, oldPos);
+        Vector3[] positions = new Vector3[6];
+        positions[0] = startPos;
+        positions[1] = endPos;
+        positions[2] = new Vector3(Random.Range(-5,5), Random.Range(-5, 5), Random.Range(-5, 5));
+        positions[3] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+        positions[4] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+        positions[5] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+        lr.positionCount = positions.Length+1;
+        for(int i =0;i!= positions.Length;i++)
+            addLineSegment(positions[i]);
+        lr.Simplify(0.1f);
     }
 
-    void addLineSegment(Vector3 start, Vector3 end)
+    void addLineSegment(Vector3 end)
     {
+        print(pointNum);
         GameObject coll = new GameObject("colliderBoi");
         coll.transform.parent = this.gameObject.transform;
         BoxCollider boxCollider = coll.AddComponent<BoxCollider>();
-        coll.transform.position = (start + end) / 2 + new Vector3(0, width, width)/2;
-        float dist = Vector3.Distance(start, end);
-        float hieght = (end - start).y;
-        float depth = (end - start).z;
-        float widthh = (end - start).x;
-        print("asin"+ hieght +" "+ dist); 
-        print(Mathf.Rad2Deg * Mathf.Asin(hieght / dist));
-        print(-Mathf.Rad2Deg * Mathf.Asin(depth / dist));
+        coll.transform.position = (oldPos + end) / 2; // + new Vector3(0, width, width)/2;
+        float dist = Vector3.Distance(oldPos, end);
+        float hieght = (end - oldPos).y;
+        float depth = (end - oldPos).z;
+        float widthh = (end - oldPos).x;
         int mult = 1;
+        int multz = -1;
+        
+        if (((hieght > 0) == true && (depth > 0) == true && (widthh > 0) == true))
+        {
+            print("SAME");
+            mult = -1;
+            multz = 1;
+        }
+        else if (((hieght > 0) == false && (depth > 0) == true && (widthh > 0) == true))
+        {
+            print("height neg");
+            mult = -1;
+            multz = 1;
+        }
+        else if (((hieght > 0) == true && (depth > 0) == false && (widthh > 0) == true) )
+        {
+            print("depth neg");
+            mult = -1;
+            multz = 1;
+        }
+        else if (((hieght > 0) == false && (depth > 0) == false && (widthh > 0) == true) )
+        {
+            print("width neg");
+            mult = -1;
+            multz = 1;
+        }
 
-        float xRot = Mathf.Rad2Deg * Mathf.Asin(widthh / Mathf.Sqrt(Mathf.Pow(depth,2) + Mathf.Pow(hieght, 2)));
-        float yRot = Mathf.Rad2Deg * Mathf.Asin(depth / Mathf.Sqrt(Mathf.Pow(widthh, 2) + Mathf.Pow(hieght, 2)));
-        float zRot = -Mathf.Rad2Deg * Mathf.Asin(hieght / Mathf.Sqrt(Mathf.Pow(depth, 2) + Mathf.Pow(widthh, 2)));
-        print(xRot +" "+ yRot  + " " + zRot);
-        coll.transform.localRotation = Quaternion.Euler(0, yRot, zRot); //
+
+
+
+        float xRot = Mathf.Rad2Deg * Mathf.Asin(widthh / dist);
+        float yRot = Mathf.Rad2Deg * Mathf.Asin(depth / Mathf.Sqrt(Mathf.Pow(widthh, 2) + Mathf.Pow(depth, 2)));
+        float zRot = Mathf.Rad2Deg * Mathf.Asin(hieght / dist);
+        print(xRot +" "+ yRot  + " " + zRot +" - "+ multz);
+        print(widthh + " " + depth + " " + hieght);
+        coll.transform.localRotation = Quaternion.Euler(0, mult*yRot, multz*zRot); //
         boxCollider.size = new Vector3(dist, width, width);
-        lr.SetPosition(0, start);
-        lr.SetPosition(1, end);
+        oldPos = end;
+        pointNum++;
+        
+        lr.SetPosition(pointNum, end);
     }
 
     void Update()
