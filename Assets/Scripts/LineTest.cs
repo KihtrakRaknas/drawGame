7b5656fe7acd89a901be9d5be489e3945a16156f
KihtrakRaknas <karthikandsankar@gmail.com>
2020-03-16 18:35:08 -0400

    added the abbility to draw on screen

--- Assets/Scripts/LineTest.cs
+++ Assets/Scripts/LineTest.cs
@@ -14,6 +14,7 @@ public class LineTest : MonoBehaviour
     float width = 0.45f;
     Vector3 oldPos = new Vector3(0, 0, 0);
     int pointNum = 0;
+    List<Vector3> positions = new List<Vector3>();
 
     void Start()
     {
@@ -26,76 +27,129 @@ public class LineTest : MonoBehaviour
         lr.endWidth = width;
         lr.materials[0] = Black;
         lr.SetPosition(0, oldPos);
-        Vector3[] positions = new Vector3[6];
-        positions[0] = startPos;
-        positions[1] = endPos;
-        positions[2] = new Vector3(Random.Range(-5,5), Random.Range(-5, 5), Random.Range(-5, 5));
-        positions[3] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
-        positions[4] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
-        positions[5] = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
-        lr.positionCount = positions.Length+1;
-        for(int i =0;i!= positions.Length;i++)
-            addLineSegment(positions[i]);
+
+        addLineSegment(startPos);
+        
         lr.Simplify(0.1f);
     }
 
     void addLineSegment(Vector3 end)
     {
-        print(pointNum);
-        GameObject coll = new GameObject("colliderBoi");
-        coll.transform.parent = this.gameObject.transform;
-        BoxCollider boxCollider = coll.AddComponent<BoxCollider>();
-        coll.transform.position = (oldPos + end) / 2; // + new Vector3(0, width, width)/2;
+
         float dist = Vector3.Distance(oldPos, end);
-        float hieght = (end - oldPos).y;
-        float depth = (end - oldPos).z;
-        float widthh = (end - oldPos).x;
-        int mult = 1;
-        int multz = -1;
+        if (dist != 0)
+        {
+            positions.Add(end);
+            lr.positionCount = positions.Count + 1;
+            print(pointNum);
+            GameObject coll = new GameObject("colliderBoi");
+            coll.transform.parent = this.gameObject.transform;
+            BoxCollider boxCollider = coll.AddComponent<BoxCollider>();
+            coll.transform.position = (oldPos + end) / 2; // + new Vector3(0, width, width)/2; 
+            float hieght = (end - oldPos).y;
+            float depth = (end - oldPos).z;
+            float widthh = (end - oldPos).x;
+            int mult = 1;
+            int multz = -1;
+
+            if (((hieght > 0) == true && (depth > 0) == true && (widthh > 0) == true))
+            {
+                print("SAME");
+                mult = -1;
+                multz = 1;
+            }
+            else if (((hieght > 0) == false && (depth > 0) == true && (widthh > 0) == true))
+            {
+                print("height neg");
+                mult = -1;
+                multz = 1;
+            }
+            else if (((hieght > 0) == true && (depth > 0) == false && (widthh > 0) == true))
+            {
+                print("depth neg");
+                mult = -1;
+                multz = 1;
+            }
+            else if (((hieght > 0) == false && (depth > 0) == false && (widthh > 0) == true))
+            {
+                print("width neg");
+                mult = -1;
+                multz = 1;
+            }
+
+
+
+
+            float xRot = Mathf.Rad2Deg * Mathf.Asin(widthh / dist);
+            float yRot;
+            if (depth!=0)
+                yRot = Mathf.Rad2Deg * Mathf.Asin(depth / Mathf.Sqrt(Mathf.Pow(widthh, 2) + Mathf.Pow(depth, 2)));
+            else
+                yRot = 0;
+            float zRot = Mathf.Rad2Deg * Mathf.Asin(hieght / dist);
+            print(""+ depth +"/"+  "Mathf.Sqrt(" + Mathf.Pow(widthh, 2) +"+" +Mathf.Pow(depth, 2) );
+            coll.transform.localRotation = Quaternion.Euler(0, mult * yRot, multz * zRot); //
+            boxCollider.size = new Vector3(dist, width, width);
+            oldPos = end;
+            pointNum++;
+
+            lr.SetPosition(pointNum, end);
+        }
         
-        if (((hieght > 0) == true && (depth > 0) == true && (widthh > 0) == true))
-        {
-            print("SAME");
-            mult = -1;
-            multz = 1;
-        }
-        else if (((hieght > 0) == false && (depth > 0) == true && (widthh > 0) == true))
-        {
-            print("height neg");
-            mult = -1;
-            multz = 1;
-        }
-        else if (((hieght > 0) == true && (depth > 0) == false && (widthh > 0) == true) )
-        {
-            print("depth neg");
-            mult = -1;
-            multz = 1;
-        }
-        else if (((hieght > 0) == false && (depth > 0) == false && (widthh > 0) == true) )
-        {
-            print("width neg");
-            mult = -1;
-            multz = 1;
-        }
-
-
-
-
-        float xRot = Mathf.Rad2Deg * Mathf.Asin(widthh / dist);
-        float yRot = Mathf.Rad2Deg * Mathf.Asin(depth / Mathf.Sqrt(Mathf.Pow(widthh, 2) + Mathf.Pow(depth, 2)));
-        float zRot = Mathf.Rad2Deg * Mathf.Asin(hieght / dist);
-        print(xRot +" "+ yRot  + " " + zRot +" - "+ multz);
-        print(widthh + " " + depth + " " + hieght);
-        coll.transform.localRotation = Quaternion.Euler(0, mult*yRot, multz*zRot); //
-        boxCollider.size = new Vector3(dist, width, width);
-        oldPos = end;
-        pointNum++;
-        
-        lr.SetPosition(pointNum, end);
     }
+
+    Vector2 oldInput;
+    bool firstClick = false;
 
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
+            if (touch.phase == TouchPhase.Began)
+            {
+                oldInput = touch.position;
+                //List<Vector3> positions = new List<Vector3>();
+                GetComponent<Rigidbody>().isKinematic = false;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 relitivePos = touch.position - oldInput;
+
+                addLineSegment(new Vector3(relitivePos.x, relitivePos.y, 0));
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (firstClick == true) {
+                firstClick = false;
+                oldInput = Input.mousePosition;
+                GetComponent<Rigidbody>().isKinematic = false;
+            }
+            else
+            {
+                Vector2 relitivePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - oldInput;
+                relitivePos /= Screen.width/10;
+
+                addLineSegment(new Vector3(relitivePos.x, relitivePos.y, 0));
+            }
+            print("CLICK");
+        }
+        else
+        {
+            if (!firstClick)
+            {
+                firstClick = true;
+                GetComponent<Rigidbody>().isKinematic = true;
+            }
+            
+        }
     }
 }
