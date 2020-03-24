d3e0bcb96a85b5760e684f426026d626ccb5ce70
KihtrakRaknas <karthikandsankar@gmail.com>
2020-03-24 15:19:40 -0400

    added moving cubes

---
+++ Assets/Scripts/SlidingCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingCube : MonoBehaviour
+{
+    public float maxPos;
+    public bool x;
+    float speed = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var pos = transform.position.x;
+        if (!x)
+            pos = transform.position.z;
+        if (Mathf.Abs(pos) > maxPos)
+        {
+            speed = -500 * pos/ Mathf.Abs(pos);
+        }
+        print(speed);
+        var vel = new Vector3(0f, 0f, speed * Time.deltaTime);
+        if (x)
+            vel =  new Vector3(speed * Time.deltaTime, 0f, 0f);
+        print(vel);
+        this.GetComponent<Rigidbody>().velocity = vel;
+    }
+}
